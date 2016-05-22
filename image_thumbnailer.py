f585028ff9960ac88a59d9ac49495dbdb5b3e812
Ubuntu16.04 <fra.morelli@gmail.com>
2016-05-22 01:12:36 -0700

    convert command

--- image_thumbnailer.py
+++ image_thumbnailer.py
@@ -3,14 +3,17 @@
 import record
 import fetcher
 import os
+import sys
 
 rs = record.AllRecords()
 f = fetcher.Fetcher('images', 0)
 rs = [r for r in rs if (r.photo_url and f.InCache(r.photo_url))]
+#rs = [r for r in rs if (r.photo_url and f.InCache(r.photo_url))]
 
 for idx, r in enumerate(rs):
   in_image = f.CacheFile(r.photo_url)
   out_image = 'thumbnails/%s.jpg' % r.photo_id()
-  cmd = 'convert %s -resize 200x200 %s' % (in_image, out_image)
+  cmd = 'convert "%s" -resize 200x200 %s' % (in_image, out_image)
+  sys.stderr.write(cmd+"\n")
   print '%05d %s' % (idx, cmd)
   os.system(cmd)
